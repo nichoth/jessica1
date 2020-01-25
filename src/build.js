e49ced02a4f9c6d2a89ef87e9d5acac0c84a96ed
nichoth <nichoth@gmail.com>
2020-01-25 13:21:12 -0800

    parse yaml

--- src/build.js
+++ src/build.js
@@ -1,1 +1,13 @@
-console.log(process.argv.slice(2))
+// console.log(process.argv.slice(2))
+var fs = require('fs')
+var fm = require('front-matter')
+
+// get all paths to yaml
+process.argv.slice(2).forEach(function(vid) {
+    fs.readFile('./' + vid, 'utf8', function (err, data) {
+        if (err) throw err
+        var yaml = fm(data)
+        console.log(yaml.attributes)
+    })
+})
+
